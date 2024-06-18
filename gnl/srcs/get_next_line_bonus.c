b30b88bb5791f096d2d0de2073d3213b3954aa72
toukoum <raphaelgiraud12@gmail.com>
2024-06-18 15:57:02 +0200

    gnl fix

--- gnl/srcs/get_next_line_bonus.c
+++ gnl/srcs/get_next_line_bonus.c
@@ -6,122 +6,125 @@
 /*   By: rgiraud <rgiraud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 13:51:20 by rgiraud           #+#    #+#             */
-/*   Updated: 2024/04/23 13:54:45 by rgiraud          ###   ########.fr       */
+/*   Updated: 2024/06/18 15:49:37 by rgiraud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/get_next_line_bonus.h"
 
-/* Returns the result of strjoin of str + buffer & frees str */
-char	*ft_joinbuffer(char *str, char buffer[BUFFER_SIZE + 1])
-{
-	char	*line;
-
-	if (buffer == NULL)
-		return (NULL);
-	else if (str == NULL)
-		return (ft_strdup(buffer));
-	line = ft_strjoin(str, buffer);
-	free(str);
-	return (line);
-}
-
-/* 	Read from a file BUFFER_SIZE bytes at a time.
-	If EOF or \n is found, return string of all read content.
-	If no bytes were read and there's nothing in BUFFER, return NULL */
-char	*read_line(char buffer[BUFFER_SIZE + 1], int fd)
-{
-	char	*line;
-	int		bytes_read;
-
-	if (ft_strchr(buffer, '\n'))
-		return (ft_strdup(buffer));
-	line = ft_strdup(buffer);
-	if (line == NULL)
-		return (NULL);
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (!bytes_read && !buffer[0])
-		return (free(line), NULL);
-	while (bytes_read > 0)
-	{
-		line = ft_joinbuffer(line, buffer);
-		if (line == NULL)
-			return (NULL);
-		if (ft_strchr(buffer, '\n'))
-			return (line);
-		ft_bzero(buffer, BUFFER_SIZE);
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-	}
-	return (line);
-}
-
-/*	Return a copied allocated string of READ_RESULT up to and including \n
-	If no \n is found, simply returns a duplicated READ_RESULT */
-char	*get_line(char *read_result)
-{
-	char	*line;
-	int		line_size;
-
-	if (read_result == NULL)
-		return (NULL);
-	line_size = 0;
-	while (read_result[line_size] && read_result[line_size] != '\n')
-		line_size++;
-	if (read_result[line_size] == '\0')
-		return (ft_strdup(read_result));
-	line_size++;
-	line = malloc(sizeof(char) * (line_size + 1));
-	if (line == NULL)
-		return (NULL);
-	ft_strlcpy(line, read_result, line_size + 1);
-	return (line);
-}
-
-/*	Remove last line content of BUFFER and get start of next line in place
-	If there's nothing after new line, sets all of buffer to 0 */
-void	reset_buffer(char buffer[BUFFER_SIZE + 1])
-{
-	int	len;
-	int	i;
-
-	len = 0;
-	while (buffer[len] && buffer[len] != '\n')
-		len++;
-	if (buffer[len] == '\0')
-		return (ft_bzero(buffer, BUFFER_SIZE));
-	len++;
-	i = 0;
-	while (buffer[len + i])
-	{
-		buffer[i] = buffer[len + i];
-		i++;
-	}
-	while (i <= BUFFER_SIZE)
-	{
-		buffer[i] = '\0';
-		i++;
-	}
-}
-
-/* 	Returns the next line of file representated by FD.
-	Line is allocated in memory and should be free'd by caller
-	Returns NULL if EOF or on error. */
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
-	char		*read_result;
-	char		*line;
+	static char	remain[FD_SIZE][BUFFER_SIZE];
+	int			b_read;
+	t_buff_ctrl	buff_ctrl;
+	t_nl		nl;
 
-	read_result = NULL;
-	if (fd == -1 || BUFFER_SIZE < 1 || read(fd, read_result, 0) == -1)
+	if (fd < 0 || fd > FD_SIZE || BUFFER_SIZE <= 0)
 		return (NULL);
-	read_result = read_line(buffer, fd);
-	if (read_result == NULL)
+	if (is_line(remain[fd], &nl, BUFFER_SIZE))
+		return (extract_remaining_line(remain[fd], nl.index));
+	init_t_buff_ctrl(&buff_ctrl);
+	if (!buff_ctrl.first_node)
 		return (NULL);
-	line = get_line(read_result);
-	free(read_result);
-	if (line == NULL)
+	buff_ctrl.node = buff_ctrl.first_node;
+	b_read = read(fd, buff_ctrl.node->buff, BUFFER_SIZE);
+	if (check_failure(buff_ctrl, b_read, gnl_strlen(remain[fd])))
 		return (NULL);
-	reset_buffer(buffer);
-	return (line);
+	while (!is_line(buff_ctrl.node->buff, &nl, b_read) && b_read == BUFFER_SIZE)
+	{
+		if (check_failure(add_back(&buff_ctrl), b_read, gnl_strlen(remain[fd])))
+			return (NULL);
+		b_read = read(fd, buff_ctrl.node->buff, BUFFER_SIZE);
+	}
+	return (build_line(buff_ctrl, remain[fd], nl, b_read));
+}
+
+char	*build_line(t_buff_ctrl buff_ctrl, char *remain, t_nl nl, int b_read)
+{
+	t_build_l	bl;
+
+	bl.i = 0;
+	bl.b_remain = b_read - (nl.index + 1);
+	buff_ctrl.node = buff_ctrl.first_node;
+	if (nl.is_found)
+		b_read = nl.index + 1;
+	bl.line = malloc(sizeof(char) * ((buff_ctrl.node_counter - 1) * BUFFER_SIZE
+				+ gnl_strlen(remain) + b_read + 1));
+	if (!bl.line)
+		return (bl.line);
+	bl.i = gnl_strlcpy(bl.line, remain, gnl_strlen(remain));
+	while (buff_ctrl.node_counter-- > 1)
+	{
+		bl.i += gnl_strlcpy(bl.line + bl.i, buff_ctrl.node->buff, BUFFER_SIZE);
+		buff_ctrl.node = buff_ctrl.node->next;
+	}
+	bl.i += gnl_strlcpy(bl.line + bl.i, buff_ctrl.node->buff, b_read);
+	if (nl.is_found)
+		gnl_strlcpy(remain, buff_ctrl.node->buff + nl.index + 1, bl.b_remain);
+	else
+		remain[0] = '\0';
+	remain[bl.b_remain] = '\0';
+	bl.line[bl.i] = '\0';
+	return (free_t_buffer_chunk(buff_ctrl.first_node), bl.line);
+}
+
+unsigned int	check_failure(t_buff_ctrl buff_ctrl, int b_read,
+		size_t len_remain)
+{
+	if (b_read <= 0 && !len_remain)
+	{
+		free_t_buffer_chunk(buff_ctrl.first_node);
+		return (1);
+	}
+	if (!buff_ctrl.node)
+	{
+		free_t_buffer_chunk(buff_ctrl.first_node);
+		return (1);
+	}
+	return (0);
+}
+
+char	*extract_remaining_line(char *remain, size_t nl_index)
+{
+	char	*extracted_line;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	extracted_line = malloc(sizeof(char) * (nl_index + 2));
+	if (!extracted_line)
+		return (extracted_line);
+	while (i != nl_index + 1)
+	{
+		extracted_line[i] = remain[i];
+		i++;
+	}
+	extracted_line[i] = '\0';
+	while (remain[i])
+		remain[j++] = remain[i++];
+	remain[j] = remain[i];
+	return (extracted_line);
+}
+
+unsigned int	is_line(char *buffer, t_nl *nl, size_t b_read)
+{
+	size_t	i;
+
+	i = 0;
+	nl->is_found = 0;
+	nl->index = 0;
+	if (!(*buffer))
+		return (nl->is_found);
+	while (i < b_read && buffer[i])
+	{
+		if (buffer[i] == '\n')
+		{
+			nl->is_found = 1;
+			nl->index = i;
+			break ;
+		}
+		i++;
+	}
+	return (nl->is_found);
 }
